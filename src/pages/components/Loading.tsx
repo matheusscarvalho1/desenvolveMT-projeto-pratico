c8ed76f32aad608523c4a7e7cd03f70820c154de
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-08-28 16:24:11 -0400

    feat: Ensure loading page works with differents cases

--- src/pages/components/Loading.tsx
+++ src/pages/components/Loading.tsx
@@ -1,16 +1,33 @@
 import { Spinner } from "../../components/ui/shadcn-io/spinner";
-import Footer from "./Footer";
-import Header from "./Header";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
-const Loading = () => {
-  return (
+interface LoadingProps {
+  fullPage?: boolean;
+  size?: number;
+}
+
+const Loading = ({ fullPage, size }: LoadingProps) => {
+  const content = (
+    <div className="flex flex-1 items-center justify-center">
+      <Spinner variant="circle-filled" className="text-blue-500" size={size} />
+    </div>
+  );
+
+  return fullPage ? (
     <div className="flex h-screen flex-col bg-gray-50">
       <Header />
       <div className="flex flex-1 items-center justify-center">
-        <Spinner variant="ellipsis" className="text-blue-500" size={40} />
+        <Spinner
+          variant="circle-filled"
+          className="text-blue-500"
+          size={size}
+        />
       </div>
       <Footer />
     </div>
+  ) : (
+    content
   );
 };
 
