477c5327f03bcc0a25a2bcadfa18d5d9c444efce
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-08-28 16:21:49 -0400

    feat: Deleting tags <a> header component

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -3,22 +3,15 @@ const Header = () => {
     <header className="bg-gradient-to-r from-blue-600 via-blue-500 to-blue-600 text-white shadow-lg">
       <div className="container mx-auto flex items-center justify-between p-4">
         <div className="flex items-center space-x-2">
-          <h1 className="text-2xl font-bold tracking-wide">Desenvolve MT</h1>
+          <h1 className="text-2xl font-bold tracking-wide">
+            <a
+              href="/"
+              className="transition-colors duration-200 hover:text-gray-200"
+            >
+              Polícia Judiciária Civil de Mato Grosso
+            </a>
+          </h1>
         </div>
-        <nav className="flex items-center space-x-6">
-          <a
-            href="/"
-            className="transition-colors duration-200 hover:text-gray-200"
-          >
-            Home
-          </a>
-          <a
-            href="/details"
-            className="transition-colors duration-200 hover:text-gray-200"
-          >
-            Details page
-          </a>
-        </nav>
       </div>
     </header>
   );
