c08fd038c61ee4735e71ad07f19e1c356d085e4d
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-08-28 19:48:44 -0400

    Feat: Adjusting loading component

--- src/pages/components/Loading.tsx
+++ src/pages/components/Loading.tsx
@@ -3,18 +3,11 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 interface LoadingProps {
-  fullPage?: boolean;
   size?: number;
 }
 
-const Loading = ({ fullPage, size }: LoadingProps) => {
-  const content = (
-    <div className="flex flex-1 items-center justify-center">
-      <Spinner variant="circle-filled" className="text-blue-500" size={size} />
-    </div>
-  );
-
-  return fullPage ? (
+const Loading = ({ size }: LoadingProps) => {
+  return (
     <div className="flex h-screen flex-col bg-gray-50">
       <Header />
       <div className="flex flex-1 items-center justify-center">
@@ -26,8 +19,6 @@ const Loading = ({ fullPage, size }: LoadingProps) => {
       </div>
       <Footer />
     </div>
-  ) : (
-    content
   );
 };
 
