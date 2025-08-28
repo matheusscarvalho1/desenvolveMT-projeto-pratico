2c70abef5164c9b5121e61f298f2e3ef16c718e6
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-08-28 19:50:04 -0400

    Fix: Adjusting styles from header

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="bg-gradient-to-r from-blue-600 via-blue-500 to-blue-600 text-white shadow-lg">
+    <header className="bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 py-6 text-gray-300 shadow-lg">
       <div className="container mx-auto flex items-center justify-between p-4">
         <div className="flex items-center space-x-2">
           <h1 className="text-2xl font-bold tracking-wide">
