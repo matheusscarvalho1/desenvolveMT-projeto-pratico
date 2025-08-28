75c22bacf51f694492758d8bb8d71b30f850239e
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-08-28 19:49:10 -0400

    Feat: Adding data from API interfaces

---
+++ src/interface/interface.ts
@@ -0,0 +1,35 @@
+export interface OcorrenciaCartazDTO {
+  urlCartaz: string;
+  tipoCartaz:
+    | "PDF_DESAPARECIDO"
+    | "PDF_LOCALIZADO"
+    | "JPG_DESAPARECIDO"
+    | "JPG_LOCALIZADO"
+    | "INSTA_DESAPARECIDO"
+    | "INSTA_LOCALIZADO";
+}
+
+export interface OcorrenciaEntrevDesapDTO {
+  informacao: string;
+  vestimentasDesaparecido: string;
+}
+
+export interface OcorrenciaDTO {
+  dtDesaparecimento: string;
+  dataLocalizacao: string;
+  encontradoVivo: boolean;
+  localDesaparecimentoConcat: string;
+  ocoId: number;
+  ocorrenciaEntrevDesapDTO: OcorrenciaEntrevDesapDTO;
+  listaCartaz: OcorrenciaCartazDTO[];
+}
+
+export interface PersonDTO {
+  id: number;
+  nome: string;
+  idade: number;
+  sexo: "MASCULINO" | "FEMININO";
+  vivo: boolean;
+  urlFoto?: string;
+  ultimaOcorrencia?: OcorrenciaDTO;
+}
