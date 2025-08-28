cd453f5777d17b58528d81a3984a612424e4379c
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-08-28 16:25:48 -0400

    feat: Adding data from api on page 'datails' and 'home'

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,17 +1,149 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { toast } from "sonner";
+
+import { Badge } from "../../components/ui/badge";
+import { api } from "../../lib/api";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Loading from "../components/Loading";
+import InternalServerError from "../Error/internal-server-error";
+import NotFound from "../Error/not-found-error";
+
+interface PessoaDTO {
+  id: number;
+  idade: number;
+  nome: string;
+  sexo: string;
+  ultimaOcorrencia?: {
+    dtDesaparecimento?: string;
+    dataLocalizacao?: string | null;
+    encontradoVivo?: boolean;
+    vestimentasDesaparecido?: string;
+  };
+  urlFoto?: string;
+  vivo: boolean;
+}
 
 const Details = () => {
+  const { id } = useParams<{ id: string }>();
+  const [data, setData] = useState<PessoaDTO>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchPessoaDetails = async () => {
+      setLoading(true);
+      try {
+        const response = await api.get<PessoaDTO>(`/pessoas/${id}`);
+        console.log(response.data);
+        setData(response.data);
+      } catch {
+        const errorMsg = "Não foi possível carregar os detalhes dessa pessoa";
+        setError(errorMsg);
+        toast.error(errorMsg);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPessoaDetails();
+  }, [id]);
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (!data?.id || Number(id) !== data.id) {
+    return <NotFound />;
+  }
+
+  if (error) {
+    <InternalServerError />;
+  }
+
   return (
     <div className="flex min-h-screen flex-col bg-gray-50">
       <Header />
-      <main className="flex flex-grow items-center justify-center p-8">
-        <div className="rounded-xl bg-white p-10 shadow-lg">
-          <h1 className="text-center text-3xl font-semibold text-gray-800">
-            Details Page
-          </h1>
+
+      <div className="container mx-auto flex-grow px-4 py-8">
+        {/* Dados da Pessoa */}
+        <div className="mx-auto max-w-2xl rounded-lg bg-white p-6 shadow">
+          <h1 className="mb-6 text-2xl font-bold text-gray-800">{data.nome}</h1>
+
+          <div className="space-y-4">
+            <div className="flex gap-6">
+              {data.urlFoto && (
+                <div>
+                  <img
+                    src={data.urlFoto}
+                    alt={data.nome}
+                    className="mt-2 max-w-xs rounded"
+                  />
+                </div>
+              )}
+              <div className="flex flex-col space-y-1">
+                {" "}
+                <div>
+                  <strong>Idade:</strong> {data.idade} anos
+                </div>
+                <div>
+                  <span>
+                    <strong>Sexo:</strong>{" "}
+                    {data.sexo
+                      ? data.sexo[0].toUpperCase() +
+                        data.sexo.slice(1).toLowerCase()
+                      : "Não informado"}
+                  </span>
+                </div>
+                <div>
+                  <strong>Status</strong>:{" "}
+                  <Badge
+                    variant="default"
+                    className={`rounded-full px-3 py-1 text-sm font-semibold ${data.vivo ? "bg-green-300 text-green-800" : "bg-red-300 text-red-800"}`}
+                  >
+                    {data.vivo ? "Localizada" : "Desaparecida"}
+                  </Badge>
+                </div>
+              </div>
+            </div>
+
+            {/* Dados da última ocorrência */}
+            {data.ultimaOcorrencia && (
+              <div className="mt-6 border-t pt-4">
+                <h2 className="mb-3 text-xl font-semibold">
+                  Última Ocorrência
+                </h2>
+
+                {data.ultimaOcorrencia.dtDesaparecimento && (
+                  <div>
+                    <strong>Data do desaparecimento:</strong>{" "}
+                    {new Date(
+                      data.ultimaOcorrencia.dtDesaparecimento,
+                    ).toLocaleDateString("pt-BR")}
+                  </div>
+                )}
+
+                {data.ultimaOcorrencia.dataLocalizacao && (
+                  <div>
+                    <strong>Data da localização:</strong>{" "}
+                    {new Date(
+                      data.ultimaOcorrencia.dataLocalizacao,
+                    ).toLocaleDateString("pt-BR")}
+                  </div>
+                )}
+
+                {data.ultimaOcorrencia.vestimentasDesaparecido && (
+                  <div>
+                    <strong>Vestimentas:</strong>{" "}
+                    {data.ultimaOcorrencia.vestimentasDesaparecido}
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
         </div>
-      </main>
+      </div>
+
       <Footer />
     </div>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,131 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
+
+import { Badge } from "../../components/ui/badge";
+import { Button } from "../../components/ui/button";
+import { api } from "../../lib/api";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Loading from "../components/Loading";
+import InternalServerError from "../Error/internal-server-error";
 
 const Home = () => {
+  interface Pessoa {
+    id: number;
+    idade: number;
+    nome: string;
+    sexo: string;
+    ultimaOcorrencia?: {
+      dtDesaparecimento?: string;
+      dataLocalizacao?: string | null;
+      encontradoVivo?: boolean;
+      vestimentasDesaparecido?: string;
+    };
+    urlFoto?: string;
+    vivo: boolean;
+  }
+
+  const navigate = useNavigate();
+  const [data, setData] = useState<Pessoa[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async (pageNumber: number = 0) => {
+    setLoading(true);
+    try {
+      const response = await api.get("/pessoas/aberto/filtro", {
+        params: { page: pageNumber, size: 10 },
+      });
+
+      setData(response.data.content);
+      //setTotalPages(response.data.totalPages);
+
+      console.log(response.data.content);
+    } catch {
+      setError("Erro na requisição de dados, statusCode: 500");
+      toast.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleDetails = (id: number) => {
+    navigate(`/details/${id}`);
+  };
+  if (loading) return <Loading fullPage size={40} />;
+  if (error) return <InternalServerError />;
+
   return (
-    <div className="flex min-h-screen flex-col bg-gray-50">
+    <div>
       <Header />
-      <div className="flex flex-grow items-center justify-center">
-        <h1 className="text-center text-3xl font-semibold text-gray-800">
-          Home Page
-        </h1>
+      <div className="container mx-auto flex-1 px-4 py-8">
+        <div className="mb-8">
+          <h1 className="mb-2 text-3xl font-bold text-gray-800">
+            Pessoas Desaparecidas
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {data.map((pessoa) => (
+            <div
+              key={pessoa.id}
+              className="overflow-hidden rounded-xl border border-gray-700 shadow-md transition-shadow duration-300 hover:shadow-lg"
+            >
+              <div className="h-90 w-full p-6">
+                <img
+                  src={pessoa.urlFoto}
+                  alt={pessoa.nome}
+                  className="h-full w-full transform rounded-xl object-cover transition-transform duration-300 ease-in-out hover:scale-105"
+                />
+              </div>
+              <div className="space-y-2 p-5">
+                <Badge
+                  variant="default"
+                  className={`rounded-full px-3 py-1 text-sm font-semibold ${pessoa.vivo ? "bg-green-300 text-green-800" : "bg-red-300 text-red-800"}`}
+                >
+                  {pessoa.vivo ? "Localizada" : "Desaparecida"}
+                </Badge>
+                <h2 className="mb-2 truncate text-xl font-bold text-gray-800">
+                  {pessoa.nome}
+                </h2>
+                <div className="flex flex-col space-y-1">
+                  <div className="flex items-center">
+                    <span className="text-muted-foreground text-sm font-medium">
+                      Sexo:{" "}
+                      {pessoa.sexo[0] + pessoa.sexo.slice(1).toLowerCase()}
+                    </span>
+                  </div>
+                  <div className="flex items-center">
+                    <span className="text-muted-foreground text-sm font-medium">
+                      Idade: {pessoa.idade} anos
+                    </span>
+                  </div>
+                  {pessoa.ultimaOcorrencia?.dtDesaparecimento && (
+                    <div className="flex items-center">
+                      <span className="text-muted-foreground text-sm font-medium">
+                        Desaparecido em:{" "}
+                        {new Date(
+                          pessoa.ultimaOcorrencia.dtDesaparecimento,
+                        ).toLocaleDateString("pt-BR")}
+                      </span>
+                    </div>
+                  )}
+                </div>
+                <Button
+                  variant="default"
+                  className="mt-4 w-full cursor-pointer rounded-lg px-4 py-2 font-medium transition-colors duration-200"
+                  onClick={() => handleDetails(pessoa.id)}
+                >
+                  Ver Detalhes
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
