cc52fe37a11a8801ad6f24aa5d13f8fa59c4aea8
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-08-29 18:19:58 -0400

    Fix: Adjusting responsivity

--- src/pages/Home/components/PersonCard.tsx
+++ src/pages/Home/components/PersonCard.tsx
@@ -19,7 +19,7 @@ const PersonCard = ({ pessoa }: PersonCardProps) => {
       key={pessoa.id}
       className="overflow-hidden rounded-xl border border-gray-700 bg-white shadow-md transition-shadow duration-300 hover:shadow-lg"
     >
-      <div className="flex h-90 w-full items-center justify-center p-6">
+      <div className="flex h-90 w-full items-center justify-center p-6 text-sm">
         {pessoa.urlFoto ? (
           <img
             src={pessoa.urlFoto}
@@ -39,23 +39,23 @@ const PersonCard = ({ pessoa }: PersonCardProps) => {
         >
           {pessoa.vivo ? "Localizada" : "Desaparecida"}
         </Badge>
-        <h2 className="mb-2 truncate text-xl font-bold text-gray-800">
+        <h2 className="mb-2 truncate text-lg font-bold text-gray-800">
           {pessoa.nome}
         </h2>
         <div className="flex flex-col space-y-1">
           <div className="flex items-center">
-            <span className="text-muted-foreground text-md font-medium">
+            <span className="text-muted-foreground text-sm font-medium">
               Sexo: {pessoa.sexo[0] + pessoa.sexo.slice(1).toLowerCase()}
             </span>
           </div>
           <div className="flex items-center">
-            <span className="text-muted-foreground text-md font-medium">
+            <span className="text-muted-foreground text-sm font-medium">
               Idade: {pessoa.idade} anos
             </span>
           </div>
           {pessoa.ultimaOcorrencia?.dtDesaparecimento && (
             <div className="flex items-center">
-              <span className="text-muted-foreground text-md font-medium">
+              <span className="text-muted-foreground text-sm font-medium">
                 Desaparecido em:{" "}
                 {new Date(
                   pessoa.ultimaOcorrencia.dtDesaparecimento,
@@ -66,7 +66,7 @@ const PersonCard = ({ pessoa }: PersonCardProps) => {
         </div>
         <Button
           variant="default"
-          className="mt-4 w-full cursor-pointer rounded-lg px-4 py-2 font-medium transition-colors duration-200"
+          className="mt-4 w-full cursor-pointer rounded-lg px-4 py-2 text-sm font-medium transition-colors duration-200"
           onClick={() => handleDetails(pessoa.id)}
         >
           Ver Detalhes
